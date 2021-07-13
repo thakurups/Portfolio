c94640b7ca7269a4e3900584f24449315638b8bd
Umesh Pratap Singh <umesh.singh_cs18@gla.ac.in>
2021-07-13 10:25:03 +0530

    projects updated

--- src/Projects.js
+++ src/Projects.js
@@ -3,38 +3,49 @@ import './Projects.css'
 export default function Projects() {
     return (
 
-            <div class="pro-contain" id="proj">
+            <div className="pro-contain" id="proj">
   
             <h1 className="title">Recent Projects</h1>
-            <div class="projects">
-            <a href="https://vipulgupta22.github.io/Friends-Restraunt/" target="_blank">
-                    <div class="card1">
-                        <div class="overlay">
-                            <div class="p-txt">Restaurant Website<br/><span>Click to Visit.</span></div>
+            <div className="projects">
+                <a href="https://vipulgupta22.github.io/Friends-Restraunt/" target="_blank">
+                    <div className="card1" id="pro1">
+                        <div className="overlay">
+                            <div className="p-txt">Restaurant Website<br/><span>Click to Visit.</span></div>
                         </div>
                     </div>
-                </a><a href="https://vipulgupta22.github.io/Friends-Restraunt/" target="_blank">
-                    <div class="card1">
-                        <div class="overlay">
-                            <div class="p-txt">Restaurant Website<br/><span>Click to Visit.</span></div>
+                </a>
+                <a href="https://tusharsaxena3112.github.io/Giphy_Searcher/index.html#" target="_blank">
+                    <div className="card1" id="pro2">
+                        <div className="overlay">
+                            <div className="p-txt">Giffy Searcher<br/><span >Click to Visit.</span></div>
                         </div>
                     </div>
-                </a><a href="https://vipulgupta22.github.io/Friends-Restraunt/" target="_blank">
-                    <div class="card1">
-                        <div class="overlay">
-                            <div class="p-txt">Restaurant Website<br/><span>Click to Visit.</span></div>
+                </a>
+                <a href="https://thakurups.github.io/Portfolio/" target="_blank">
+                    <div className="card1" id="pro3">
+                        <div className="overlay">
+                            <div className="p-txt">Portfolio website<br/><span>Click to Visit.</span></div>
                         </div>
                     </div>
-                </a><a href="https://vipulgupta22.github.io/Friends-Restraunt/" target="_blank">
-                    <div class="card1">
-                        <div class="overlay">
-                            <div class="p-txt">Restaurant Website<br/><span>Click to Visit.</span></div>
+                </a>
+                <a href="#" target="_blank">
+                    <div className="card1">
+                        <div className="overlay">
+                            <div className="p-txt">Uploading soon<br/><span>Click to Visit.</span></div>
                         </div>
                     </div>
-                </a><a href="https://vipulgupta22.github.io/Friends-Restraunt/" target="_blank">
-                    <div class="card1">
-                        <div class="overlay">
-                            <div class="p-txt">Restaurant Website<br/><span>Click to Visit.</span></div>
+                </a>
+                <a href="#" target="_blank">
+                    <div className="card1">
+                        <div className="overlay">
+                            <div className="p-txt">Uploading soon<br/><span>Click to Visit.</span></div>
+                        </div>
+                    </div>
+                </a>
+                <a href="#" target="_blank">
+                    <div className="card1">
+                        <div className="overlay">
+                            <div className="p-txt">Uploading soon<br/><span>Click to Visit.</span></div>
                         </div>
                     </div>
                 </a>
